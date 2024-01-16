886c7c9f275a30ccc6d93f9169b8c262d3e8d529
jcastano1997 <jcastano1997@gmail.com>
2024-01-16 17:36:10 -0500

    Base project

---
+++ src/views/LandingPage.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+import { CustomButton } from "@components/common/material-ui/CustomButton";
+import { BaseLayout } from "@layouts/BaseLayout";
+
+export function LandingPage() {
+	return (
+		<BaseLayout>
+			<h1>Landing Page</h1>
+			<Link to="/account">
+				<CustomButton>Go to account</CustomButton>
+			</Link>
+			<Link to="/digitalSign">
+				<CustomButton>Go to digital sign</CustomButton>
+			</Link>
+			<Link to="/customEntity">
+				<CustomButton>Go to custom entity</CustomButton>
+			</Link>
+			<Link to="/connectFour">
+				<CustomButton>Go to connectFour</CustomButton>
+			</Link>
+		</BaseLayout>
+	);
+}
